14fe8c1ffee5c8dbf7443cbd2c8ee40ec30b3325
Ganesh <stsiyer@gmail.com>
2024-05-22 23:12:55 +0530

    modified log

--- backend/src/index.js
+++ backend/src/index.js
@@ -41,4 +41,4 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(port, () => console.log(`Server listening on port ${port} 123`));
