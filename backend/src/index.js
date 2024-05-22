87ef6d7a7dbb27cfcb24e869d8d054d586a61dde
Ganesh <stsiyer@gmail.com>
2024-05-22 21:25:52 +0530

    modified log

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,20 +2,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require("dotenv").config();
-const userRoutes =require('./routes/usersRoute.js');
-const walletRoutes =require('./routes/walletRoute.js');
-const productRoutes =require('./routes/productRoute.js');
-const bidderRoutes =require('./routes/bidderRoute.js');
+const userRoutes = require('./routes/usersRoute.js');
+const walletRoutes = require('./routes/walletRoute.js');
+const productRoutes = require('./routes/productRoute.js');
+const bidderRoutes = require('./routes/bidderRoute.js');
 const app = express();
 const port = process.env.PORT || 5000; // Use environment variable for port
 const connectDb = require("./config/dbConnection.js");
 connectDb();
+
+// CORS configuration
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Parse incoming request bodies
 app.use(bodyParser.json());
-app.use(cors());
 
+// Routes
 app.use('/user', userRoutes);
 app.use('/wallet', walletRoutes);
 app.use('/product', productRoutes);
-app.use('/product',bidderRoutes);
-app.listen(port, () => console.log(`Server listening on port ${port} n`));
+app.use('/bidder', bidderRoutes); // Fix the endpoint for bidderRoutes
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
