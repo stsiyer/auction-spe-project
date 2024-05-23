eb9cf47680f1610859b5b765329b296a9e11ae6a
stsiyer <stsiyer@gmail.com>
2024-05-23 12:32:34 +0530

    corrected name

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ganesh/Desktop/auction-spe-project/venv"
+set -gx VIRTUAL_ENV "/home/stsiyer/Desktop/auction-spe-project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
